d05496f8d2cbd024a242872602737d71e5303eeb
Polomb1 <20060711roma@mail.ru>
2026-05-14 11:33:47 +0300

    Revert "Полная реализация"
    This reverts commit f97c428ac35a7019506750eb66b2854eba6a6ec5.

--- src/mainer.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "mainer.h"
-#include <stdexcept>
-
-Calculator::Calculator()
-{
-    sintaksis = std::make_unique<SintaksisAnalizer>();
-    translator = std::make_unique<TranslatorPolski>();
-}
-
